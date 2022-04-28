92f8d0be3e7a92b3548be897d5b2dc616bed72cc
Dylan <dylan.vallee@hotmail.com>
2022-04-28 17:46:53 +0200

    Added account management on profile page
    The user can change their personal information and password

--- purbeurre/account/forms.py
+++ purbeurre/account/forms.py
@@ -1,6 +1,31 @@
+from django import forms
+from django.forms.utils import ErrorList
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
+
+
+class CustomErrorList(ErrorList):
+    def __init__(self, initlist=None, error_class=None):
+        if error_class is None:
+            error_class = 'mb-1'
+        super(CustomErrorList, self).__init__(initlist=initlist, error_class=error_class)
+        
+
+class UserUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        self.error_class = CustomErrorList
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        error_messages = {
+            'email': {
+                'unique': _('Cette adresse email est déjà utilisée.')
+            }
+        }
 
 
 class RegisterUserForm(UserCreationForm):
